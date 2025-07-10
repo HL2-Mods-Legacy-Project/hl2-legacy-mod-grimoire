c06e627f56824ecfd3e7424c67ffc973b289886b
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:01:16 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Grimoire_1.01" `
+  -ModName "Grimoire 1.01" `
+  -ModFolder "Azure" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/grimoire/Grimoire-1.01-Original.zip" `
+  -ModBaseFilesUrlHash "5701b328b074d4e77eee489afdcdfee0571c1821362e7334d8559ce5a70816a6" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
